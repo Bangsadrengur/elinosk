f88af907c5011c5804eff6edf10d45915c77344b
Heimir Þór Kjartansson <hemmi.hk@gmail.com>
2011-05-20 20:44:42 +0000

    ADD: lots

---
+++ elinosk/urls.py
@@ -0,0 +1,25 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'welcome.views.index'),
+    # url(r'^$', 'elinosk.views.home', name='home'),
+    # url(r'^elinosk/', include('elinosk.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
+
+from django.conf import settings
+if settings.DEBUG:
+     urlpatterns += patterns('',
+     url(r'%s(?P<path>.*)/$' % settings.MEDIA_URL[1:], 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, }),
+)
+
