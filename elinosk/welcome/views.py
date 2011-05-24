86cfdaf9f3f1b35583faa6973707fee31c5077c6
Heimir Þór Kjartansson <hemmi.hk@gmail.com>
2011-05-24 14:01:41 +0000

    Mass update

--- elinosk/welcome/views.py
+++ elinosk/welcome/views.py
@@ -2,10 +2,10 @@
 from django.shortcuts import render
 
 def index(request):
-    dirka = ("Þetta er heimasíða óperusöngkonunnar Elínar Óskar Óskarsdóttur. "
+    Befni = ("Þetta er heimasíða óperusöngkonunnar Elínar Óskar Óskarsdóttur. "
              "Hér á þessari síðu geturðu lesið um feril minn og fundið " 
              "myndir af mér hvort sem er í hlutverki á óperusviðinu og ýmsar "
              "aðrar myndir sem skýra sig sjálfar. Þú getur hlustað á "
              "tónlistina mína og horft á klippur úr nokkrum vel völdum óperum.")
 
-    return render(request, 'base.html', {'dirka': dirka})
+    return render(request, 'base_w.html', {'Befni': Befni})
